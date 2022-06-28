2b1f0041eb9efdc741d82bedbc5ea5f7fb90f796
endlishemmet <endlishem@gmail.com>
2022-06-28 19:41:55 +0300

    improvizing code

--- src/hdap.py
+++ src/hdap.py
@@ -1,5 +1,3 @@
-from concurrent.futures import wait
-from locale import resetlocale
 from bs4 import BeautifulSoup
 from rich.console import Console
 from rich.theme import Theme
@@ -20,6 +18,8 @@ def error(err, err_type, line='std'):
     )[red];[/red]""")
     quit()
 
+def warning(warn):
+    console.print(f"[blue]warning[/blue]: {warn}")
 
 try:
     file_name = ARGS[0]
@@ -31,82 +31,41 @@ if 'file_name' in globals():
         error(f'file extension not supportable for usage.\n\t use one of file extension in {file_extensions}', 'file_extension')
     
     try:
+        FUNCTIONS = {
+            "math"
+        }
         with open(file_name) as f:
-            content = f.read()
+            content = f.read().strip()
             f.close()
         
-        if not content.strip():
-            quit()
-        else:
-            FUNCTIONS = {
-                'pow': {
-                    'op': '**'
-                },
-                'add': {
-                    'op': '+'
-                },
-                'sub': {
-                    'op': '-'
-                },
-                'divm': {
-                    'op': '/'
-                },
-                'mod': {
-                    'op': '%'
-                },
-                'mult': {
-                    'op': '*'
-                }
-            }
-            VARIABLES = {}
-            soup = BeautifulSoup(content, 'html.parser')
+        soup = BeautifulSoup(content, 'html.parser')
 
-            saves = soup.find_all("save")
-            for save in saves:
-                for var in save.attrs:
-                    VARIABLES[var] = {}
-                    VARIABLES[var]["value"] = save.attrs[var]
-                    try:
-                        VARIABLES[var]["type"]  = re.findall(r"^\<class \'(.*)\'\>$", str(eval(f"type({save.attrs[var]})")))[0]
-                    except SyntaxError:
-                        del VARIABLES[var]
-                        error(f'cannot to set {var}\'s type.', 'variable_type')
-            
-            for var in VARIABLES:
-                try:
-                    soup.find(var).replaceWith(VARIABLES[var]["value"])
-                except AttributeError:
-                    pass
-            
-            for func in FUNCTIONS:
-                
-                func_list = soup.find_all(func)
-                if len(func_list) > 1:
-                    for _func in func_list:
-                        arg_list = []
-                        res = FUNCTIONS[func]["op"].join(arg_list)
+        for FUNCTION in FUNCTIONS:
+            match FUNCTION:
+                case "math":
+                    math_tags = soup.find_all('math')
 
-                        print(eval(res)) if res else ''
-                else:
-                    try:
-                        _func = func_list[0]
-                        res = FUNCTIONS[func]["op"].join([_func.attrs[i] for i in _func.attrs])
-                        
-                        try:
-                            for var in VARIABLES:
-                                res = res.replace(f"@{var}", VARIABLES[var]["value"])
-                            soup.find(_func.name, attrs=_func.attrs).replaceWith(str(eval(res)))
-                        except SyntaxError:
-                            error(f'cannot operate that operation', "operation")
-                        except TypeError: 
-                            error('cannot operate that operation', "operation")
-                    except IndexError:
-                        pass
-            
-            with open(file_name, "w+") as f:
-                f.write(soup.prettify())
-                f.close()
-            
+                    if math_tags:
+                        for math_tag in math_tags:
+                            if math_tag.get('op'):
+                                op = math_tag.attrs["op"]
+                                
+                                if op not in (math_ops := ['+', '-', '*', '/', '//', '%', '**']):
+                                    warning("your math tag's <op> attribute value doesnt in math operator list.")
+                                else:
+                                    res = op.join([math_tag.attrs[i] for i in list(math_tag.attrs) if i != "op"])
+                                    
+                                    try:
+                                        soup.find(math_tag.name, attrs=math_tag.attrs).replaceWith(str(eval(res)))
+                                    except SyntaxError:
+                                        error(f"cannot do that operation: {res}", "op_error")
+                            else:
+                                warning("math tag's <op> attribute must to get a value.")
 
+                    else:
+                        continue
+        with open(file_name, "w+") as f:
+            f.write(soup.prettify())
+            f.close()
     except FileNotFoundError:
         error(f'file with name {file_name} not found.', 'file_not_found')
